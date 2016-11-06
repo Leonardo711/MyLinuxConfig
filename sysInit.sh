8f6860bddf885236ce8aec91db2010530ed163bf
leo <leo@gmail.com>
2016-11-07 01:04:12 +0800

    add new file

---
+++ sysInit.sh
@@ -0,0 +1,48 @@
+## delete the first "#" mark, then run
+
+## Install git
+#sudo apt-get install git
+#
+## Install oh-my-zsh
+#sudo apt-get install zsh
+#chsh -s /bin/zsh
+#wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+#echo "alias cls='clear' 
+#alias ll='ls -l' 
+#alias la='ls -a' 
+#alias vi='vim' 
+#alias -s html=mate
+#alias -s py=vi 
+#alias -s js=vi 
+#alias -s c=vi 
+#alias -s java=vi 
+#alias -s txt=vi 
+#alias -s gz='tar -xzvf' 
+#alias -s tgz='tar -xzvf' 
+#alias -s zip='unzip' 
+#alias -s bz2='tar -xjvf'" >> ~/.zshrc
+
+#sudo apt-get install tree
+
+## python package
+#sudo apt-get install python-pip
+#sudo pip install ipython notebook
+#sudo pip install --upgrade pip
+#sudo pip install --upgrade jupyter
+#sudo pip install django==1.9.2
+#sudo pip install pandas
+#sudo pip install sklearn
+#sudo pip install xgboost
+#sudo pip install numpy
+#sudo pip install scipy
+#sudo pip install matplotlib
+#
+#vim configuration
+#sudo apt-get install vim
+##sudo mkdir -p  ~/.vim/bundle
+##cd ~/.vim/bundle
+##sudo git clone https://github.com/gmarik/vundle.git
+#sudo curl -O https://raw.githubusercontent.com/vince67/v7_config/master/vim.sh
+#bash vim.sh
+
+
